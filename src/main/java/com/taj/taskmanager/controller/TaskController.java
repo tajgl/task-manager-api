1af52318c923ac39477a95697f2e644c55f9711f
Taj <tajveergill11@gmail.com>
2026-01-14 18:36:23 -0800

    Buisness logic added to service layer

--- src/main/java/com/taj/taskmanager/controller/TaskController.java
+++ src/main/java/com/taj/taskmanager/controller/TaskController.java
@@ -5,7 +5,9 @@ import com.taj.taskmanager.model.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/tasks")
@@ -29,13 +31,18 @@ public class TaskController {
     }
 
     @GetMapping(path = "{taskId}")
-    public Task getTaskById(@PathVariable("taskId") Long taskId) {
+    public Optional<Task> getTaskById(@PathVariable("taskId") Long taskId) {
         return taskService.getTaskById(taskId);
     }
 
     @PutMapping(path = "{taskId}")
-    public Task updateTask(@PathVariable("taskId") Long taskId, @RequestBody Task task) {
-        return taskService.updateTask(taskId, task);
+    public Task updateTask(@PathVariable("taskId") Long taskId,
+                           @RequestParam(required = false) String title,
+                           @RequestParam(required = false) String description,
+                           @RequestParam(required = false) Task.Priority priority,
+                           @RequestParam(required = false) LocalDate dueDate,
+                           @RequestParam(required = false) Task.Status status) {
+        return taskService.updateTask(taskId, title, description, priority, dueDate, status);
     }
 
     @DeleteMapping(path = "{taskId}")
--- src/main/java/com/taj/taskmanager/service/TaskService.java
+++ src/main/java/com/taj/taskmanager/service/TaskService.java
@@ -5,12 +5,15 @@ import com.taj.taskmanager.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class TaskService {
 
-    private TaskRepository taskRepository;
+    private final TaskRepository taskRepository;
 
     @Autowired
     public TaskService(TaskRepository taskRepository) {
@@ -18,21 +21,42 @@ public class TaskService {
     }
 
     public Task createTask(Task task) {
-        return null;
+        return taskRepository.save(task);
     }
 
     public List<Task> getAllTasks() {
-        return null;
+        return taskRepository.findAll();
     }
 
-    public Task getTaskById(long id) {
-        return null;
+    public Optional<Task> getTaskById(long id) {
+        return taskRepository.findById(id);
     }
 
-    public Task updateTask(Long taskId, Task task) {
-        return null;
+    public Task updateTask(Long taskId, String title, String description, Task.Priority priority, LocalDate dueDate, Task.Status status) {
+        Task task = taskRepository.findById(taskId).orElseThrow(() -> new IllegalStateException("Task does not exist"));
+        if (title != null && !title.isEmpty() && !Objects.equals(task.getTitle(), title)) {
+            task.setTitle(title);
+        }
+        if (description != null && !description.isEmpty() && !Objects.equals(task.getDescription(), description)) {
+            task.setDescription(description);
+        }
+        if (priority != null && !Objects.equals(task.getPriority(), priority)) {
+            task.setPriority(priority);
+        }
+        if (dueDate != null && !Objects.equals(task.getDueDate(), dueDate)) {
+            task.setDueDate(dueDate);
+        }
+        if (status != null && !Objects.equals(task.getStatus(), status)) {
+            task.setStatus(status);
+        }
+
+        return task;
     }
 
     public void deleteTask(Long taskId) {
+        if (taskRepository.findById(taskId).isEmpty()) {
+            throw new IllegalStateException("Task with id: " + taskId + " does not exist");
+        }
+        taskRepository.deleteById(taskId);
     }
 }
