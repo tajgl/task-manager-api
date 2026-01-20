8bce73131c8d0477b998d24d30fb25ad06b3a66c
Taj <tajveergill11@gmail.com>
2026-01-19 20:01:42 -0800

    Project controller added

---
+++ src/main/java/com/taj/taskmanager/controller/ProjectController.java
@@ -0,0 +1,54 @@
+package com.taj.taskmanager.controller;
+
+import com.taj.taskmanager.model.Project;
+import com.taj.taskmanager.model.Task;
+import com.taj.taskmanager.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/projects")
+public class ProjectController {
+
+    private final ProjectService projectService;
+
+    @Autowired
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Project> createProject(@RequestBody Project project) {
+        return new ResponseEntity<>(projectService.createProject(project), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Project>> getAllProjects() {
+        return new ResponseEntity<>(projectService.getAllProjects(), HttpStatus.OK);
+    }
+
+    @GetMapping(path="{projectId}")
+    public ResponseEntity<Project> getProjectById(@PathVariable Long projectId) {
+        return new ResponseEntity<>(projectService.getProjectById(projectId), HttpStatus.OK);
+    }
+
+    @PutMapping(path="{projectId}")
+    public ResponseEntity<Project> updateProject(@PathVariable Long projectId, @RequestBody Project project) {
+        return new ResponseEntity<>(projectService.updateProject(projectId, project), HttpStatus.OK);
+    }
+
+    @DeleteMapping(path="{projectId}")
+    public ResponseEntity<Void> deleteProject(@PathVariable Long projectId) {
+        projectService.deleteProject(projectId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping(path="{projectId}/tasks")
+    public ResponseEntity<List<Task>>  getTasksByProjectId(@PathVariable Long projectId) {
+        return new ResponseEntity<>(projectService.getTasksByProjectId(projectId), HttpStatus.OK);
+    }
+}
