86d285bd16af93e0c013104906c66ed3e94bc455
Taj <tajveergill11@gmail.com>
2026-04-26 00:30:39 -0700

    Update SecurityConstants to read from application.yml instead of directly

--- src/main/java/com/taj/taskmanager/security/SecurityConstants.java
+++ src/main/java/com/taj/taskmanager/security/SecurityConstants.java
@@ -1,7 +1,19 @@
 package com.taj.taskmanager.security;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SecurityConstants {
 
-    public static final long JWT_EXPIRATION = 70000;
-    public static final String JWT_SECRET = "secret";
+    public static String JWT_SECRET;
+    public static long JWT_EXPIRATION;
+
+    @Value("${jwt.secret}")
+    public void setJwtSecret(String secret) {
+        JWT_SECRET = secret;
+    }
+
+    @Value("${jwt.expiration}")
+    public void setJwtExpiration(long expiration) {
+        JWT_EXPIRATION = expiration;
+    }
 }
