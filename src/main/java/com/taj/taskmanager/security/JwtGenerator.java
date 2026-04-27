a3b4826de6fe88bd8fafe576ccd98ae0d90259d0
Taj <tajveergill11@gmail.com>
2026-04-26 23:24:36 -0700

    Updated JwtGenerator to inject security values from within the class

--- src/main/java/com/taj/taskmanager/security/JwtGenerator.java
+++ src/main/java/com/taj/taskmanager/security/JwtGenerator.java
@@ -3,6 +3,7 @@ package com.taj.taskmanager.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -12,22 +13,28 @@ import java.util.Date;
 @Component
 public class JwtGenerator {
 
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
+
     public String generateToken(Authentication authentication) {
         String username = authentication.getName();
         Date currentDate = new Date();
-        Date expiryDate = new Date(currentDate.getTime() + SecurityConstants.JWT_EXPIRATION);
+        Date expiryDate = new Date(currentDate.getTime() + jwtExpiration);
 
         return Jwts.builder()
                 .subject(username)
                 .issuedAt(new Date())
                 .expiration(expiryDate)
-                .signWith(SignatureAlgorithm.HS512, SecurityConstants.JWT_SECRET)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .compact();
     }
 
     public String getUsernameFromToken(String token) {
         Claims claims = Jwts.parser()
-                .setSigningKey(SecurityConstants.JWT_SECRET)
+                .setSigningKey(jwtSecret)
                 .build()
                 .parseSignedClaims(token)
                 .getPayload();
@@ -38,7 +45,7 @@ public class JwtGenerator {
     public boolean validateToken(String token) {
         try {
             Jwts.parser()
-                    .setSigningKey(SecurityConstants.JWT_SECRET)
+                    .setSigningKey(jwtSecret)
                     .build()
                     .parseClaimsJws(token);
             return true;
